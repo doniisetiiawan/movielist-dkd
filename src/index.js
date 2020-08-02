1c7238520750700126661494128a4b0bcde76484
Doni Setiawan <donisetiawan.050@gmail.com>
2020-08-02 20:06:31 +0700

    Setting up a project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <h1>movieLists</h1>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
+
+module.exports = {
+  devServer: {
+    compress: true,
+    port: 3000,
+    stats: 'errors-only',
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+};
