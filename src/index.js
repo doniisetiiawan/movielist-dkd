6809ce8b7ca6f8d0d99a78d3f82ce698e977ab27
Doni Setiawan <donisetiawan.050@gmail.com>
2020-08-02 20:39:35 +0700

    Structuring a project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import List from './containers/list';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => <h1>movieLists</h1>;
+const App = () => (
+  <div className="container-fluid">
+    <nav className="navbar sticky-top navbar-light bg-dark">
+      <h1 className="navbar-brand text-light">MovieList</h1>
+    </nav>
+
+    <List />
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
     stats: 'errors-only',
     open: true,
   },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   module: {
     rules: [
       {
@@ -20,6 +23,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
